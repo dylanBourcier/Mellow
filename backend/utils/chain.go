0a200fa30f728a2eb592261d3d739946c4ec3667
FredericTischler <frederic.tischler2@gmail.com>
2025-06-25 17:21:01 +0200

    Server + Logger & CORS middlewares

--- backend/utils/chain.go
+++ backend/utils/chain.go
@@ -11,3 +11,13 @@ func Chain(h http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
 	}
 	return h
 }
+
+// Middleware pour http.Handler
+type HTTPMiddleware func(http.Handler) http.Handler
+
+func ChainHTTP(h http.Handler, middlewares ...HTTPMiddleware) http.Handler {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+	return h
+}
