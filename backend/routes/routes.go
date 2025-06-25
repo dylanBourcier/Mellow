29c0d5daf3f99357c109b7f8959b973d84fe4027
FredericTischler <frederic.tischler2@gmail.com>
2025-06-25 17:00:12 +0200

    Router global + fonction utilitaire pour chainer des middlewares

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,1 +1,40 @@
 package routes
+
+import (
+	"net/http"
+
+	"mellow/routes/admin"
+	"mellow/routes/auth"
+	"mellow/routes/groups"
+	"mellow/routes/messages"
+	"mellow/routes/notifications"
+	"mellow/routes/posts"
+	"mellow/routes/users"
+)
+
+func SetupRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Authentification
+	auth.RegisterAuthRoutes(mux)
+
+	// Utilisateurs
+	users.RegisterUserRoutes(mux)
+
+	// Publications (posts)
+	posts.RegisterPostRoutes(mux)
+
+	// Groupes
+	groups.RegisterGroupRoutes(mux)
+
+	// Notifications
+	notifications.RegisterNotificationRoutes(mux)
+
+	// Messages privés + groupes
+	messages.RegisterMessageRoutes(mux)
+
+	// Modération (admin)
+	admin.RegisterAdminRoutes(mux)
+
+	return mux
+}
---
+++ backend/utils/chain.go
@@ -0,0 +1,13 @@
+package utils
+
+import "net/http"
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// Fonction permettant de chainer plusieurs middleware
+func Chain(h http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+	return h
+}
