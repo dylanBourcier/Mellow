53fc3d5dca9652cbc839c6d6f38b55dba560bcbf
FredericTischler <frederic.tischler2@gmail.com>
2025-06-26 14:15:46 +0200

    users & posts models

--- backend/models/post.go
+++ backend/models/post.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Post struct {
+	PostID       uuid.UUID  `json:"post_id"`
+	GroupID      *uuid.UUID `json:"group_id,omitempty"` // nullable
+	UserID       uuid.UUID  `json:"user_id"`
+	Title        string     `json:"title"`
+	Content      string     `json:"content"`
+	CreationDate time.Time  `json:"creation_date"`
+	Visibility   string     `json:"visibility"`
+}
