b091f8f158f90cc5f5ab5b3fd25c2d93f4a1e1a6
FredericTischler <frederic.tischler2@gmail.com>
2025-06-27 11:07:19 +0200

    admin_repo setup

--- backend/repositories/admin_repository.go
+++ backend/repositories/admin_repository.go
@@ -1,1 +1,16 @@
+// File: mellow/repositories/admin_repository.go
 package repositories
+
+import (
+	"context"
+	"mellow/models"
+)
+
+type AdminRepository interface {
+	GetAllUsers(ctx context.Context) ([]*models.User, error)
+	UpdateUserRole(ctx context.Context, userID string, newRole string) error
+	DeleteUser(ctx context.Context, userID string) error
+
+	GetAllReports(ctx context.Context) ([]models.Report, error)
+	DeleteReportedContent(ctx context.Context, reportID string) error
+}
