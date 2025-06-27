0d674e738906b87dece9f6b97a463ba28ce5d5c1
FredericTischler <frederic.tischler2@gmail.com>
2025-06-27 10:12:25 +0200

    auth_service setup

--- backend/services/auth_service.go
+++ backend/services/auth_service.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"context"
+	"mellow/models"
+)
+
+type AuthService interface {
+	// Login vérifie les identifiants et crée une session.
+	Login(ctx context.Context, username, password string) (*models.User, error)
+
+	// Logout détruit la session utilisateur.
+	Logout(ctx context.Context, sessionID string) error
+
+	// IsAuthenticated vérifie si une session est valide.
+	IsAuthenticated(ctx context.Context, sessionID string) (bool, error)
+
+	// GetUserFromSession retourne l'utilisateur lié à une session.
+	GetUserFromSession(ctx context.Context, sessionID string) (*models.User, error)
+}
