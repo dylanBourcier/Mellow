14164620a7a526a6e385af6f85249dc7e912e617
dbourcie <dylan.bourcier.27@gmail.com>
2025-06-30 16:23:29 +0200

    Color variables and fonts

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,11 @@
-
 import React from 'react';
 
 export default function Home() {
   return (
-   <div>Hello</div>
+    <div>
+      <h1 className="font-heading text-lavender-1 tracking-tighter text-4xl mb-4">
+        Hello world
+      </h1>
+    </div>
   );
 }
