465be4f048e9fadce55e7ea4af6e90ffeee3d80a
ajoly <alexandre.joly.76300@gmail.com>
2025-07-07 10:47:20 +0200

    PostCreation page

--- frontend/src/app/components/layout/PostCreationForm.jsx
+++ frontend/src/app/components/layout/PostCreationForm.jsx
@@ -4,11 +4,21 @@ import Label from '../ui/Label';
 import Input from '../ui/Input';
 
 function PostCreationForm() {
+    const visibilityOptions=[
+        { value: 'public', label: 'Public' },
+        { value: 'private', label: 'Private' },
+        { value: 'almost-private', label: 'Almost Private' },
+    ]
+    const whoCanSeeOptions=[
+        { value: 'everyone', label: 'Everyone' },
+        { value: 'group', label: 'group' },
+        { value: 'group2', label: 'group2' },
+    ]
     return (
         <form className='flex flex-col gap-2.5 w-full max-w-[600px]'>
             <div>
-                <Label htmlFor={"visibility-select"}>Post On* :</Label>
-                <InputSelect></InputSelect>
+                <Label htmlFor={"postOn"}>Post On* :</Label>
+                <InputSelect id="postOn" options={whoCanSeeOptions}></InputSelect>
             </div>
             <div>
                 <Label htmlFor={"title"}>Title* :</Label>
@@ -27,9 +37,30 @@ function PostCreationForm() {
 
             </div>
             <div>
-                <Label htmlFor={""}></Label>
-                <InputSelect></InputSelect>
+                <Label htmlFor={"visibility-select"}></Label>
+                <div className='flex gap-1 justify-center'>
+                    <div className='flex-1'>
+                    <input type='radio' name="visibility" id="public" className='hidden peer'/>
+                    <label htmlFor="public" className='flex align-middle justify-center py-2 border border-lavender-5 text-lavender-5 bg-transparent rounded-2xl peer-checked:bg-lavender-6 cursor-pointer transition-colors duration-200'>Public</label>
+                    </div>
+                    <div className='flex-1'>
+                    <input type='radio' name="visibility" id="almost-private" className='hidden peer'/>
+                    <label htmlFor="almost-private" className='flex align-middle justify-center py-2 border border-lavender-5 text-lavender-5 bg-transparent rounded-2xl peer-checked:bg-lavender-6 cursor-pointer transition-colors duration-200'>Almost Private</label>
+                    </div>
+                    <div className='flex-1'>
+                    <input type='radio' name="visibility" id="private" className='hidden peer'/>
+                    <label htmlFor="private" className='flex align-middle justify-center py-2 border border-lavender-5 text-lavender-5 bg-transparent rounded-2xl peer-checked:bg-lavender-6 cursor-pointer transition-colors duration-200'>Private</label>
+                    </div>
+                </div>
             </div>
+            <div>
+                <Label htmlFor={"image"}>Image :</Label>
+                <Input type="file" id="image" accept="image/*"></Input>
+            </div>
+            {/* <div>
+                <Label htmlFor={""}>Who can see your post* :</Label>
+                <InputSelect></InputSelect>
+            </div> */}
         </form>
     );
 }
--- frontend/src/app/components/ui/InputSelect.jsx
+++ frontend/src/app/components/ui/InputSelect.jsx
@@ -1,21 +1,17 @@
 "use client";
 import React from 'react';
 
-function InputSelect() {
-    const options = [
-        {value: 'Everyone', label: 'Everyone'
-        },
-        {value: 'Group', label: 'Group'},
-        {value: 'Group Admin', label: 'Group Admin'},
-        {value: 'Group Members', label: 'Group Members'},
-    ]
+function InputSelect({options}, id) {
     const handleChange = (event) => {
         console.log(event.target.value);
     };
+    console.log(options);
+    
     return (
         <div>
             <select
-                id="visibility-select"
+                id={id}
+                name={id}
                 onChange={handleChange}
                 className="block w-full p-2 bg-white rounded-lg shadow-(--box-shadow) focus:outline-none focus:ring-2 focus:ring-lavender-4 focus:border-lavender-4"
             >
