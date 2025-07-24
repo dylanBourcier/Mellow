35f54bc3a9a5c28bb70fa3bfbe307eaf2e9a4ac6
SpauriRosso <46430869+SpauriRosso@users.noreply.github.com>
2025-07-24 16:42:33 +0200

    Added private message controller

--- backend/controllers/messages/private.go
+++ backend/controllers/messages/private.go
@@ -1,11 +1,73 @@
 package messages
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"mellow/models"
+	"mellow/services"
+	"mellow/utils"
+	"net/http"
+)
 
-func GetConversation(w http.ResponseWriter, r *http.Request, userId string) {
-	// TODO: retourner les messages échangés avec l’utilisateur
+func GetConversation(service services.MessageService, userId string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		uid, err := utils.GetUserIDFromContext(r.Context())
+		if err != nil {
+			utils.RespondError(w, http.StatusUnauthorized, "Unauthorized", utils.ErrUnauthorized)
+			return
+		}
+
+		page := 1
+		pageSize := 20
+		msgs, err := service.GetConversation(r.Context(), uid.String(), userId, page, pageSize)
+		if err != nil {
+			utils.RespondError(w, http.StatusInternalServerError, "Failed to get conversation", err)
+			return
+		}
+		utils.RespondJSON(w, http.StatusOK, "Conversation retrieved", msgs)
+	}
 }
 
-func SendMessage(w http.ResponseWriter, r *http.Request, userId string) {
-	// TODO: envoyer un message privé
+func SendMessage(service services.MessageService, userId string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		defer r.Body.Close()
+		uid, err := utils.GetUserIDFromContext(r.Context())
+		if err != nil {
+			utils.RespondError(w, http.StatusUnauthorized, "Unauthorized", utils.ErrUnauthorized)
+			return
+		}
+
+		var payload struct {
+			Content string `json:"content"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+			utils.RespondError(w, http.StatusBadRequest, "Invalid JSON", utils.ErrInvalidJSON)
+			return
+		}
+
+		if payload.Content == "" {
+			utils.RespondError(w, http.StatusBadRequest, "Empty content", utils.ErrInvalidPayload)
+			return
+		}
+
+		receiverID, err := uuid.Parse(userId)
+		if err != nil {
+			utils.RespondError(w, http.StatusBadRequest, "Invalid user", utils.ErrInvalidPayload)
+			return
+		}
+		
+		content := payload.Content
+		msg := models.Message{
+			SenderID:   uid,
+			ReceiverID: receiverID,
+			Content:    &content,
+		}
+		if err := service.SendMessage(r.Context(), &msg); err != nil {
+			utils.RespondError(w, http.StatusInternalServerError, "Failed to send message", err)
+			return
+		}
+		utils.RespondJSON(w, http.StatusCreated, "Message sent", msg)
+	}
 }
