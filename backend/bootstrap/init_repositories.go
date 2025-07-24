045014fe2a944f9a650ff080da80929ca98a53ba
SpauriRosso <46430869+SpauriRosso@users.noreply.github.com>
2025-07-24 15:46:23 +0200

    Initialization of messages repository
    # Conflicts:
    #	backend/bootstrap/init_repositories.go
    ## Merged mine tdo to changes for private messages

--- backend/bootstrap/init_repositories.go
+++ backend/bootstrap/init_repositories.go
@@ -7,21 +7,21 @@ import (
 )
 
 type Repositories struct {
-	UserRepository         repositories.UserRepository
-	AuthRepository         repositories.AuthRepository
-	PostRepository         repositories.PostRepository
-	GroupRepository        repositories.GroupRepository
-	CommentRepository      repositories.CommentRepository
-	NotificationRepository repositories.NotificationRepository
+	UserRepository    repositories.UserRepository
+	AuthRepository    repositories.AuthRepository
+	PostRepository    repositories.PostRepository
+	GroupRepository   repositories.GroupRepository
+	CommentRepository repositories.CommentRepository
+	MessageRepository repositories.MessageRepository
 }
 
 func InitRepositories(db *sql.DB) *Repositories {
 	return &Repositories{
-		UserRepository:         repoimpl.NewUserRepository(db),
-		AuthRepository:         repoimpl.NewAuthRepository(db),
-		PostRepository:         repoimpl.NewPostRepository(db),
-		GroupRepository:        repoimpl.NewGroupRepository(db),
-		CommentRepository:      repoimpl.NewCommentRepository(db),
-		NotificationRepository: repoimpl.NewNotificationRepository(db),
+		UserRepository:    repoimpl.NewUserRepository(db),
+		AuthRepository:    repoimpl.NewAuthRepository(db),
+		PostRepository:    repoimpl.NewPostRepository(db),
+		GroupRepository:   repoimpl.NewGroupRepository(db),
+		CommentRepository: repoimpl.NewCommentRepository(db),
+		MessageRepository: repoimpl.NewMessageRepository(db),
 	}
 }
--- backend/routes/messages/messages.go
+++ backend/routes/messages/messages.go
@@ -1,11 +1,14 @@
 package messages
 
 import (
+	"mellow/middlewares"
+	"mellow/services"
+	"mellow/utils"
 	"net/http"
 )
 
-func RegisterMessageRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/messages/", MessageUserRouter) // GET|POST /messages/:userId
-
+func RegisterMessageRoutes(mux *http.ServeMux, msgService services.MessageService, authService services.AuthService) {
+	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService)).ServeHTTP) // GET|POST /messages/:userId
+	// TODO: Implement group + logic
 	mux.HandleFunc("/messages/group/", MessageGroupRouter) // GET|POST /messages/group/:groupId
 }
