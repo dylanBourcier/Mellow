b09702f81d581df4782920b07f3a74457051f3f8
SpauriRosso <46430869+SpauriRosso@users.noreply.github.com>
2025-07-24 16:33:54 +0200

    added necessary message route

--- backend/routes/messages/messages.go
+++ backend/routes/messages/messages.go
@@ -8,7 +8,11 @@ import (
 )
 
 func RegisterMessageRoutes(mux *http.ServeMux, msgService services.MessageService, authService services.AuthService) {
+<<<<<<< HEAD
 	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService)).ServeHTTP) // GET|POST /messages/:userId
+=======
+	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService))) // GET|POST /messages/:userId
+>>>>>>> dece5e0 (added necessary message route)
 	// TODO: Implement group + logic
 	mux.HandleFunc("/messages/group/", MessageGroupRouter) // GET|POST /messages/group/:groupId
 }
