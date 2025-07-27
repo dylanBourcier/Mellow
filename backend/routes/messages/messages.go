1f51205b11d66c32f909122a9c3a0d3160755596
SpauriRosso <46430869+SpauriRosso@users.noreply.github.com>
2025-07-28 00:17:35 +0200

    Update profil handler

--- backend/routes/messages/messages.go
+++ backend/routes/messages/messages.go
@@ -8,11 +8,9 @@ import (
 )
 
 func RegisterMessageRoutes(mux *http.ServeMux, msgService services.MessageService, authService services.AuthService) {
-<<<<<<< HEAD
-	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService)).ServeHTTP) // GET|POST /messages/:userId
-=======
-	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService))) // GET|POST /messages/:userId
->>>>>>> dece5e0 (added necessary message route)
+	// GET|POST /messages/:userId
+	mux.HandleFunc("/messages/", utils.ChainHTTP(MessageUserRouter(msgService), middlewares.RequireAuthMiddleware(authService)).ServeHTTP)
 	// TODO: Implement group + logic
-	mux.HandleFunc("/messages/group/", MessageGroupRouter) // GET|POST /messages/group/:groupId
+	// GET|POST /messages/group/:groupId
+	mux.HandleFunc("/messages/group/", MessageGroupRouter)
 }
