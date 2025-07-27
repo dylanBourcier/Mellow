8d4504502466837f2978535184f532ac7dbd364f
SpauriRosso <46430869+SpauriRosso@users.noreply.github.com>
2025-07-28 00:18:16 +0200

    added read check controller

---
+++ backend/controllers/messages/read.go
@@ -0,0 +1,31 @@
+package messages
+
+import (
+	"mellow/services"
+	"mellow/utils"
+	"net/http"
+	"strings"
+)
+
+func MarkAsRead(service services.MessageService) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		msgID := strings.TrimPrefix(r.URL.Path, "/messages/read/")
+		if msgID == "" || strings.Contains(msgID, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		uid, err := utils.GetUserIDFromContext(r.Context())
+		if err != nil {
+			utils.RespondError(w, http.StatusUnauthorized, "Unauthorized", utils.ErrUnauthorized)
+			return
+		}
+
+		if err := service.MarkAsRead(r.Context(), msgID, uid.String()); err != nil {
+			utils.RespondError(w, http.StatusInternalServerError, "Failed to mark message as read", err)
+			return
+		}
+		utils.RespondJSON(w, http.StatusOK, "Message marked as read", nil)
+	}
+}
